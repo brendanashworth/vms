f3e6c8737294c0c735cbd344cf2197fbe91d9ab5
Brendan Ashworth <brendan.ashworth@me.com>
2015-03-07 15:36:40 -0800

    better setup.sh

--- freebsd/setup.sh
+++ freebsd/setup.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
 # package installs
-sudo pkg install git        \
-                 python     \
-                 gmake      \
-                 lang/gcc49
+sudo pkg install git         \
+                 python      \
+                 gmake       \
+\ # C / C++ compilers
+                 lang/gcc49  \
+		 clang36     \
+\ # back-port linux-y programs
+		 libexecinfo
 
-# to get g++, we need to symbolic link to gcc49
-sudo ln -s /usr/local/bin/gcc49 /usr/local/bin/g++
+# to get g++ and gcc, we need to symbolic link to gcc49
+sudo ln -s $(which gcc49) /usr/local/bin/g++
+sudo ln -s $(which gcc49) /usr/local/bin/gcc
+
+# get newer clang
+sudo mv /usr/bin/clang   /usr/bin/_old_clang
+sudo mv /usr/bin/clang++ /usr/bin/_old_clang++
+sudo ln -s /usr/local/bin/clang36   /usr/bin/clang
+sudo ln -s /usr/local/bin/clang++36 /usr/bin/clang++
